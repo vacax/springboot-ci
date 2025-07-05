80971533d0bf291583ad1d1401a637108a1e4570
Carlos Camacho <ca.camacho@ce.pucmm.edu.do>
2025-07-05 00:51:23 -0400

    Problema con el Key.

--- src/main/java/edu/pucmm/eict/springbootci/SpringbootCiApplication.java
+++ src/main/java/edu/pucmm/eict/springbootci/SpringbootCiApplication.java
@@ -8,6 +8,7 @@ public class SpringbootCiApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringbootCiApplication.class, args);
+        String AWS_SECRET_ACCESS_KEY = "AKIA1234567890EXAMPLEKEY";
     }
 
 }
